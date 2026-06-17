f9d2d4e0c4d724c9548b8647a65999c28ade59b4
Russell Barton <146447950+rrbarton51@users.noreply.github.com>
2026-06-17 13:13:32 -0400

    Update testrgp.R
    Limit examples to plots used in the WSC 2026 RGP tutorial by Barton

--- testrgp.R
+++ testrgp.R
@@ -1,7 +1,6 @@
 # testrgp.R
 # Russell R. Barton September 2025
-#   functions for RGP tutorial WSC `26 paper April 2026
-# Some rgp.R parameters have been adjusted from the original rgp2 code to 
+#   functions for RGP tutorial WSC `26 paper June 2026
 #  enhance plots used in the WSC 2026 RGP tutorial by Barton.
 # example program to define a function and levels for plotting
 #  then call rgp - Response Grid Plot. See
@@ -10,88 +9,6 @@
 #  Barton, R. R. (2025) Response Grid Plots for Model-Agnostic Machine Learning Insight
 
 # Make sure plot window is large enough to accommodate plot legend
-
-
-
-# EOQ model 5 levels
-
-source("rgp.R")
-f <- function(x){
-  D = x[1]
-  S = x[2]
-  H = x[3]
-  EOQ = sqrt(2*S*D/H)
-}
-fname = "EOQ 5lev"
-
-xlo = c(35,8,4)
-xhi = c(65,17,8.5)
-ylab = "EOQ"
-xlab = NULL
-xlab[1]="D";xlab[2]="S";xlab[3]="H"
-nlev = c(5,5,5)
-
-rgp(f,fname,nlev,ylab,xlab,xlo,xhi)
-
-
-# EOQ model 2 levels
-
-source("rgp.R")
-f <- function(x){
-  D = x[1]
-  S = x[2]
-  H = x[3]
-  EOQ = sqrt(2*S*D/H)
-}
-fname = "EOQ 2lev"
-
-xlo = c(40,10,5)
-xhi = c(60,15,7.5)
-ylab = "EOQ"
-xlab = NULL
-xlab[1]="D";xlab[2]="S";xlab[3]="H"
-nlev = c(2,2,2)
-
-rgp(f,fname,nlev,ylab,xlab,xlo,xhi)
-
-
-# Beeler et al. Regression form rather than ANOVA
-# (works with any nlev, here 3)
-
-source("rgp.R")
-fname = "BeelerReg"
-
-xlo = c(-1,-1,0,0)
-xhi = c(1,1,2,2)
-ylab = "Cases"
-xlab = NULL
-xlab[1] = "Days"
-xlab[2] = "Infec"
-xlab[3] = "Vac"
-xlab[4] = "Quar"
-nlev = c(3,3,3,3)
-source("BeelerReg.R")
-
-rgp(fr,fname,nlev,ylab,xlab,xlo,xhi)
-
-# Beeler et al. Regression form rather than ANOVA
-# (works with any nlev, here 4)
-
-source("rgp.R")
-fname = "BeelerReg"
-
-xlo = c(-1,-1,0,0)
-xhi = c(1,1,2,2)
-ylab = "Cases"
-xlab = NULL
-xlab[1] = "Days"
-xlab[2] = "Infec"
-xlab[3] = "Vac"
-xlab[4] = "Quar"
-nlev = c(4,4,4,4)
-source("BeelerReg.R")
-
-rgp(fr,fname,nlev,ylab,xlab,xlo,xhi)
 
 #=========================
 #=========================
@@ -138,27 +55,6 @@ rgp(f,fname,nlev,ylab,xlab,xlo,xhi)
 
 #=========================
 
-source("rgp.R")
-fname = "sS Optimality Gap (mlegp)"
-
-# for SanchezSSCONTmlegp, scaling is used for x
-
-xlo <- c(.1,.5,1,.1)
-xhi <- c(.5,.9,3,.9)
-
-ylab = "Fraction Opt. Gap"
-xlab = NULL
-xlab[1] = "eta_1"
-xlab[2] = "eta_2"
-xlab[3] = "gamma_1"
-xlab[4] = "gamma_2"
-nlev = c(3,3,5,5)
-
-# create metamodel object
-source("SanchezSSCONTmlegp.R")
-
-rgp(f,fname,nlev,ylab,xlab,xlo,xhi)
-
 # now Sanchez data SAN results
 #=====================================================
 
@@ -204,28 +100,6 @@ rgp(f,fname,nlev,ylab,xlab,xlo,xhi)
 
 #=========================
 
-source("rgp.R")
-fname = "SAN Optimality Gap (mlegp)"
-
-# for SanchezSANmlegp, scaling is used for x
-
-xlo <- c(.1,.5,1,.1)
-xhi <- c(.5,.9,3,.9)
-
-ylab = "Fraction Opt. Gap"
-xlab = NULL
-xlab[1] = "eta_1"
-xlab[2] = "eta_2"
-xlab[3] = "gamma_1"
-xlab[4] = "gamma_2"
-nlev = c(3,3,5,5)
-
-# create metamodel object
-source("SanchezSANmlegp.R")
-
-rgp(f,fname,nlev,ylab,xlab,xlo,xhi)
-
-#=========================
 #=========================
 
 # Shashaani et al. 2024 inventory area 
@@ -273,18 +147,6 @@ rgp(f,fname,nlev,ylab,xlab,xlo,xhi)
 
 # Plots for figures showing construction
 
-# simple linear plus quadratic in x[1] d = 3
-source("rgp.R")
-f <- function(x) 2 + x[1]^2 +x[2] +x[3]
-fname = "f(x) = 2 + x[1]^2 +x[2] +x[3]"
-xlo = rep(0,3)
-xhi = rep(3,3)
-ylab = "f"
-xlab = NULL
-xlab[1]="x[1]";xlab[2]="x[2]";xlab[3]="x[3]"
-nlev = c(4,3,3)
-
-rgp(f,fname,nlev,ylab,xlab,xlo,xhi)
 
 # simple linear plus quadratic in x[1] d = 4
 source("rgp.R")
@@ -308,9 +170,9 @@ xlo = rep(0,6)
 xhi = rep(2,6)
 ylab = "f"
 xlab = NULL
-xlab[1]="x[1]";xlab[2]="x[2]";xlab[3]="x[3]";xlab[4]="x[4]"
-xlab[5]="x[5]";xlab[6]="x[6]"
-nlev = c(3,3,3,2,2,2)
+xlab[1]="Var1";xlab[2]="Var2";xlab[3]="Var3";xlab[4]="Var4"
+xlab[5]="Var5";xlab[6]="Var6"
+nlev = c(2,2,2,3,3,3)
 
 rgp(f,fname,nlev,ylab,xlab,xlo,xhi)
 
